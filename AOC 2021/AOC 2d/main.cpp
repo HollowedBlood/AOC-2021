df93c95d64eb5e20746126a99568415794d5486a
HollowedBlood <hunterbaugus@gmail.com>
2021-12-20 23:10:55 -0600

    2d
    Completed the second day of the Advent of Code Calendar

---
+++ AOC 2021/AOC 2d/main.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    int distance = 0, depth = 0, aim = 0;
+    fstream newfile;
+    newfile.open("Input.txt", ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()) {   //checking whether the file is 
+        string currentText;
+
+        while (getline(newfile, currentText)) {  //read data from file object and put it into string.
+            
+            enum directions {forward, up, down};
+            directions currentDirection;
+            if (currentText.substr(0, currentText.find(" ")) == "forward") {
+                currentDirection = forward;
+            }
+            else if (currentText.substr(0, currentText.find(" ")) == "up")
+            {
+                currentDirection = up;
+            }
+            else if (currentText.substr(0, currentText.find(" ")) == "down")
+            {
+                currentDirection = down;
+            }
+
+            int movement = stoi(currentText.substr(currentText.find(" ") + 1));
+
+            switch (currentDirection)
+            {
+            default:
+                cout << "how are you even here?!" << endl;
+                break;
+            case forward:
+                cout << "forward " << movement << endl;
+                distance += movement;
+                depth += movement * aim;
+                break;
+            case up:
+                cout << "up " << movement << endl;
+                aim -= movement;
+                break;
+            case down: 
+                cout << "down " << movement << endl;
+                aim += movement;
+                break;
+            }
+
+        }
+    }
+    newfile.close();   //close the file object.
+    cout << "distance: " << distance << ", depth: " << depth << ", product: " << distance*depth << endl;
+}
