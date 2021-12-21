abdcc5c5e6f33637991d794901ff2219711f9523
HollowedBlood <hunterbaugus@gmail.com>
2021-12-20 22:08:30 -0600

    completed the first day of AOC 2021
    ugly commit but it is everything

---
+++ AOC 2021/AOC 1d/main.cpp
@@ -0,0 +1,69 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    fstream newfile;
+    int increases = 0;
+
+    newfile.open("Input.txt", ios::in); //open a file to perform read operation using file object
+    if (newfile.is_open()) {   //checking whether the file is open
+        string currentText;
+        int currentNum, lastNum, firstNum, currentSum, lastSum;
+        int index = 0;
+        
+        while (getline(newfile, currentText)) {  //read data from file object and put it into string.
+            currentNum = stoi(currentText);
+
+            if (index < 2)
+            {
+                cout << "No sums" << endl;
+            }
+            else if (index == 2)
+            {
+                currentSum = currentNum + lastNum + firstNum;
+                cout << "currentSum: " << currentSum << ", No previous sum" << endl;
+                lastSum = currentSum;
+            }
+            else
+            {
+                currentSum = currentNum + lastNum + firstNum;
+                if (currentSum > lastSum)
+                {
+                    cout << "currentSum: " << currentSum << ", Is greater than previous sum" << endl;
+                    lastSum = currentSum;
+                    increases++;
+                }
+                else if (currentSum < lastSum)
+                {
+                    cout << "currentSum: " << currentSum << ", Is less than previous sum" << endl;
+                    lastSum = currentSum;
+                }
+                else
+                {
+                    cout << "currentSum: " << currentSum << ", No change" << endl;
+                    lastSum = currentSum;
+                }
+            }
+
+
+            if (index > 0) {
+                firstNum = lastNum;
+            }            
+            lastNum = stoi(currentText);
+            index++;
+        }
+        
+        newfile.close();   //close the file object.
+    }
+
+    cout << "the total number of increases was: " << increases << endl;
+    
+}
